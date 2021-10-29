a781cbc0517f88141b72d24e76a566df724b2c77
Tkachmaxim <tkachmaxim23@gmail.com>
2021-10-29 12:56:13 +0300

    add_auth

--- TrainerApp/math_module/views.py
+++ TrainerApp/math_module/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 
 class MathInputView(View):
+    @method_decorator(login_required)
     def get(self, request):
         return render(request, 'mathtrainer.html')
+
 
 
